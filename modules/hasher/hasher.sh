d7d2d5a479abe23d23e63e50e623914346ce4e0c
atomsP <petruokat@gmail.com>
2024-05-13 00:13:16 +0300

    differ added

--- modules/hasher/hasher.sh
+++ modules/hasher/hasher.sh
@@ -22,16 +22,26 @@ for path in "${hash_locations[@]}"; do
    done < <(find "$path" -type f)
    
    for file in "${file_list[@]}"; do
+      pathname=$(echo "$path" | tr '/' '_')
       filename=$(echo "$file" | tr '/' '_')
-      local_dest="${local_dir}${filename}-${timestamp}.hash"
+      local_dest="${local_dir}${filename}.hash"
       hash=$(md5sum "$file" | awk '{print $1}')
       echo "$hash" > "${local_dest}"
    done
+
+   tar_file=${pathname}@hashes_${timestamp}.tar
+   tar cvf "${tar_file}" -C ${local_dir} .
+
+   rsync -e "ssh -p ${remote_port}" ${tar_file} ${remote_user}@${remote_host}:${remote_dest}
+
+   rm -rf ${local_dir}
+   rm -f ${tar_file}
+
+   sh ./differ.sh ${tar_file} ${remote_dest} ${timestamp}
+
 done
 
-tar_file=hashes_${timestamp}.tar
-tar cvf ${tar_file} ./hashes/
-rsync -e "ssh -p ${remote_port}" ${tar_file} ${remote_user}@${remote_host}:${remote_dest}
 
-rm -rf ${local_dir}
-rm -f ${tar_file}
+
+
+
