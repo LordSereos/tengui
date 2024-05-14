fc26234765436afd6fcbee7bfc7ebc3add6b3848
atomsP <petruokat@gmail.com>
2024-05-14 22:19:26 +0300

    audit

---
+++ modules/audit/retrieve.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+
+username="$1"
+remote_host="$2"
+port="$3"
+shift 3
+audit_locations=("$@")
+local_dir="/home/tom/MEGA/treciasKursas/tengui/modules/audit/${remote_host}/"
+timestamp=$(date +%m%d_%H%M)
+
+mkdir -p "$local_dir"
+
+ssh "$username@$remote_host" "last" >> "${local_dir}last-${timestamp}.log"
+ssh "$username@$remote_host" "lastb" >> "${local_dir}lastb-${timestamp}.log"
+ssh "$username@$remote_host" "lastlog" >> "${local_dir}lastlog-${timestamp}.log"
+ssh "$username@$remote_host" "ac -p" >> "${local_dir}ac-${timestamp}.log"
+ssh "$username@$remote_host" "lastcomm" >> "${local_dir}lastcomm-${timestamp}.log"
+
+for location in ${audit_locations[@]}; do
+   ssh "$username@$remote_host" "ausearch -k ${location}-FLAG" >> "${local_dir}ausearch-${location}-${timestamp}.log"
+done
---
+++ modules/audit/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+username="$1"
+remote_host="$2"
+port="$3"
+shift 3
+audit_locations=("$@")
+
+ssh_command="sudo touch /var/log/wtmp; sudo touch /var/log/btmp; sudo apt install acct && sudo accton on"
+#ssh_command="curl -O https://ftp.gnu.org/gnu/acct/acct-6.6.4.tar.gz"         IF NOT IN REPO   
+#scp acct-6.6.4.tar.gz $username@$remote_host:/home/$username                 PUSH IF NEEDED
+#ssh_command="cd /home/$username && tar xf acct-6.6.4.tar.gz && cd acct-6.6.4 && ./configure && sudo make install"
+
+ssh "$username@$remote_host" "$ssh_command"
+
+for location in @{audit_locations[@]}; do
+   ssh "$username@$remote_host" "sudo auditctl -w $location -p wa -k '${location}-FLAG'"
+done
